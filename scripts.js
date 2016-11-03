3f917bb825347dbed38fabe988c1a81e66eeb464
CHASE-PC\Chase <chaselose@live.com>
2016-11-03 19:14:01 -0400

    The search file was changed to a browse button and all the the card fronts were assigned to variables. Not sure if sourced properly currently.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,63 @@
 /**
  * Created by Chase on 10/26/2016.
  */
+
+//Diamonds
+var diA =   "cards/ace_of_diamonds";
+var di2 =   "cards/2_of_diamonds";
+var di3 =   "cards/3_of_diamonds";
+var di4 =   "cards/4_of_diamonds";
+var di5 =   "cards/5_of_diamonds";
+var di6 =   "cards/6_of_diamonds";
+var di7 =   "cards/7_of_diamonds";
+var di8 =   "cards/8_of_diamonds";
+var di9 =   "cards/9_of_diamonds";
+var di0 =   "cards/10_of_diamonds";
+var diJ =   "cards/jack_of_diamonds2";
+var diQ =   "cards/queen_of_diamonds2";
+var diK =   "cards/king_of_diamonds2";
+
+//Hearts
+var hrtA =  "cards/ace_of_hearts";
+var hrt2 =  "cards/2_of_hearts";
+var hrt3 =  "cards/3_of_hearts";
+var hrt4 =  "cards/4_of_hearts";
+var hrt5 =  "cards/5_of_hearts";
+var hrt6 =  "cards/6_of_hearts";
+var hrt7 =  "cards/7_of_hearts";
+var hrt8 =  "cards/8_of_hearts";
+var hrt9 =  "cards/9_of_hearts";
+var hrt0 =  "cards/10_of_hearts";
+var hrtJ =  "cards/jack_of_hearts2";
+var hrtQ =  "cards/queen_of_hearts2";
+var hrtK =  "cards/king_of_hearts2";
+
+//Clubs
+var clbA =  "cards/ace_of_clubs";
+var clb2 =  "cards/2_of_clubs";
+var clb3 =  "cards/3_of_clubs";
+var clb4 =  "cards/4_of_clubs";
+var clb5 =  "cards/5_of_clubs";
+var clb6 =  "cards/6_of_clubs";
+var clb7 =  "cards/7_of_clubs";
+var clb8 =  "cards/8_of_clubs";
+var clb9 =  "cards/9_of_clubs";
+var clb0 =  "cards/10_of_clubs";
+var clbJ =  "cards/jack_of_clubs2";
+var clbQ =  "cards/queen_of_clubs2";
+var clbK =  "cards/king_of_clubs2";
+
+//Spades
+var spdA =  "cards/ace_of_spades";
+var spd2 =  "cards/2_of_spades";
+var spd3 =  "cards/3_of_spades";
+var spd4 =  "cards/4_of_spades";
+var spd5 =  "cards/5_of_spades";
+var spd6 =  "cards/6_of_spades";
+var spd7 =  "cards/7_of_spades";
+var spd8 =  "cards/8_of_spades";
+var spd9 =  "cards/9_of_spades";
+var spd0 =  "cards/10_of_spades";
+var spdJ =  "cards/jack_of_spades2";
+var spdQ =  "cards/queen_of_spades2";
+var spdK =  "cards/king_of_spades2";
